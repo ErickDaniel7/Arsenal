63b53f14dfa03339c0e76924ec9f47bb13256c0d
Erick Daniel Teixeira Vier <erixkat10@gmail.com>
2024-09-23 10:23:35 -0300

    Delete Trabalhos/Task_Tracker directory

--- Trabalhos/Task_Tracker/src/main/java/org/example/tasktracer/Repository/TarefasRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.tasktracer.Repository;
-
-import org.example.tasktracer.Model.Tarefas;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarefasRepository extends JpaRepository<Tarefas, Integer> {
-}
